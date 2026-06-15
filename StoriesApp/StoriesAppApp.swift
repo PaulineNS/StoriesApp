594aa3bb6dfce74bec94680a8778fca7b27e9124
PaulineNs <pauline.nomballais@hotmail.fr>
2026-06-15 10:29:36 +0200

    Initial Commit

---
+++ StoriesApp/StoriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesAppApp.swift
+//  StoriesApp
+//
+//  Created by Pauline Nomballais on 15/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct StoriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
